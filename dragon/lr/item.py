44a5c9f69457eec6a79e9fc0fe1317df19347e41
Martin <martin@martin.com>
2012-03-21 11:54:44 -0300

    Refactorizados los items LR0 y LR1 para que sean clases completas.

--- dragon/lr/item.py
+++ dragon/lr/item.py
@@ -1,44 +1,49 @@
 import collections
 
-Item = collections.namedtuple('Item', ('sym_production', 'alternative', 'position'))
+class Item:
+   def __init__(self, sym_production, alternative, position):
+      self.sym_production = sym_production
+      self.alternative = alternative
+      self.position = position
 
-def _at_the_end(self, my_alternative, grammar):
-   '''Return True if the items is A -> abc* .'''
-   return len(my_alternative) == self.position or grammar.is_empty_rule(my_alternative)
+   def at_the_end(self, my_alternative, grammar):
+      '''Return True if the items is A -> abc* .'''
+      return len(my_alternative) == self.position or grammar.is_empty_rule(my_alternative)
 
-def _next_items(self, grammar):
-   '''Given the item A -> abc*Bd, return each alternative of B -> *efg.
-      Return a empty list if B is not a production or not exist B.'''
-   alternative = grammar[self.sym_production][self.alternative]
-   if self._at_the_end(alternative, grammar): # No exist 'next'
-      return []
+   def next_items(self, grammar):
+      '''Given the item A -> abc*Bd, return each alternative of B -> *efg.
+         Return a empty list if B is not a production or not exist B.'''
+      alternative = grammar[self.sym_production][self.alternative]
+      if self.at_the_end(alternative, grammar): # No exist 'next'
+         return []
 
-   next_sym_production = alternative[self.position]
-   if grammar.is_a_nonterminal(next_sym_production):
-      return [Item(next_sym_production, i, 0) for i in range(len(grammar[next_sym_production]))]
+      next_sym_production = alternative[self.position]
+      if grammar.is_a_nonterminal(next_sym_production):
+         return [Item(next_sym_production, i, 0) for i in range(len(grammar[next_sym_production]))]
 
-   else:
-      return []
-
-
-def _next_symbol(self, grammar):
-   '''Given the item A -> abc*Xde, return X.'''
-   alternative = grammar[self.sym_production][self.alternative]
-   if self._at_the_end(alternative, grammar):
-      return None
-
-   return alternative[self.position]
+      else:
+         return []
 
 
-def _item_shifted(self, grammar):
-   '''Given the item A -> abc*Xde, return a new item A -> abcX*de.
-      
-      Precondition: The initial item must not be A -> abc* .'''
-   assert self.next_symbol(grammar)
-   return Item(self.sym_production, self.alternative, self.position + 1)
+   def next_symbol(self, grammar):
+      '''Given the item A -> abc*Xde, return X.'''
+      alternative = grammar[self.sym_production][self.alternative]
+      if self.at_the_end(alternative, grammar):
+         return None
+
+      return alternative[self.position]
 
 
-Item.next_items = _next_items
-Item.next_symbol = _next_symbol
-Item.item_shifted = _item_shifted
-Item._at_the_end = _at_the_end
+   def item_shifted(self, grammar):
+      '''Given the item A -> abc*Xde, return a new item A -> abcX*de.
+         
+         Precondition: The initial item must not be A -> abc* .'''
+      assert self.next_symbol(grammar)
+      return Item(self.sym_production, self.alternative, self.position + 1)
+
+   def __hash__(self):
+      return hash((self.sym_production, self.alternative, self.position))
+
+   def __eq__(self, other):
+      return hash(self) == hash(other)
+   
--- dragon/lr/lookahead.py
+++ dragon/lr/lookahead.py
@@ -1,39 +1,42 @@
 import collections
-import dragon.lr.item
+from dragon.lr.item import Item
 from dragon.util import first
 
-LookAhead = collections.namedtuple('LookAhead', ('sym_production', 'alternative', 'position', 'lookahead'))
+class LookAhead(Item):
+   def __init__(self, sym_production, alternative, position, lookahead):
+      Item.__init__(self, sym_production, alternative, position)
+      self.lookahead = lookahead
 
-def _next_items(self, grammar):
-   '''Given the item A -> abc*Bd [x], where x is the lookahead terminal, return 
-      each alternative of B -> *efg [y], for each 'y' in first(dx).
-      Return a empty list if B is not a production or not exist B.'''
-   alternative = grammar[self.sym_production][self.alternative]
-   if self._at_the_end(alternative, grammar): # No exist 'next'
-      return []
+   def next_items(self, grammar):
+      '''Given the item A -> abc*Bd [x], where x is the lookahead terminal, return 
+         each alternative of B -> *efg [y], for each 'y' in first(dx).
+         Return a empty list if B is not a production or not exist B.'''
+      alternative = grammar[self.sym_production][self.alternative]
+      if self.at_the_end(alternative, grammar): # No exist 'next'
+         return []
 
-   next_sym_production = alternative[self.position]
-   if grammar.is_a_nonterminal(next_sym_production):
-      result = []
-      first_set = first(grammar, alternative[self.position + 1:] + (self.lookahead,)) 
-      for terminal in first_set:
-         result.extend([LookAhead(next_sym_production, i, 0, terminal) for i in range(len(grammar[next_sym_production]))])
-      
-      return result
-   else:
-      return []
-
-
-def _item_shifted(self, grammar):
-   '''Given the item A -> abc*Xde [x], return a new item A -> abcX*de [x] where x is
-      the terminal lookahead.
-      
-      Precondition: The initial item must not be A -> abc* .'''
-   assert self.next_symbol(grammar)
-   return LookAhead(self.sym_production, self.alternative, self.position + 1, self.lookahead)
+      next_sym_production = alternative[self.position]
+      if grammar.is_a_nonterminal(next_sym_production):
+         result = []
+         first_set = first(grammar, alternative[self.position + 1:] + (self.lookahead,)) 
+         for terminal in first_set:
+            result.extend([LookAhead(next_sym_production, i, 0, terminal) for i in range(len(grammar[next_sym_production]))])
+         
+         return result
+      else:
+         return []
 
 
-LookAhead.next_items = _next_items
-LookAhead.next_symbol = dragon.lr.item._next_symbol
-LookAhead.item_shifted = _item_shifted
-LookAhead._at_the_end = dragon.lr.item._at_the_end
+   def item_shifted(self, grammar):
+      '''Given the item A -> abc*Xde [x], return a new item A -> abcX*de [x] where x is
+         the terminal lookahead.
+         
+         Precondition: The initial item must not be A -> abc* .'''
+      assert self.next_symbol(grammar)
+      return LookAhead(self.sym_production, self.alternative, self.position + 1, self.lookahead)
+
+   def __hash__(self):
+      return hash((self.sym_production, self.alternative, self.position, self.lookahead))
+   
+   def __eq__(self, other):
+      return hash(self) == hash(other)
